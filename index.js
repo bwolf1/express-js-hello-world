6fa3da6025c01c72141ef8ab7b15b242152350f0
Brian Wolf <bwolf1@gmail.com>
2024-07-01 16:26:00 -0500

    Add module example

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 // Run with: node index.js
 // Access with: http://localhost:3000/
 
+const birds = require("./birds");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  res.redirect("/brian"); // 302 Found,
 });
 
 app.get("/brian", (req, res) => {
-  res.send("Hey Man!");
+  res.json({ message: "Hey Man!" });
 });
+
+app.use("/birds", birds);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
